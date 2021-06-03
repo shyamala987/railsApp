7c72c84e52dd6d232d71482c3629bc949904d107
Shyamala Venkataramani <shyamala.venkataramani@clio.com>
2021-06-03 16:15:02 -0700

    First pass

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,5 @@
+class ApplicationController < ActionController::Base
+    def welcome
+    #    render html:'Hello and welcome!'
+    end
+end
---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,6 @@
+class StaticController < ApplicationController
+    def hello
+    end
+    def about
+    end
+end
